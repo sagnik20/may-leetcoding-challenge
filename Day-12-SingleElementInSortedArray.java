e3ea38558a03603f9566ea6f22ebd837dc17c916
Sagnik Chattopadhyaya <36898274+sagnik20@users.noreply.github.com>
2020-05-13 09:51:31 +0530

    Added Day-12-SingleElementInSortedArray.java

---
+++ Day-12-SingleElementInSortedArray.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        //Length of the array would be odd or else the constraint fails
+        int n = nums.length;
+        if (n == 1) return nums[0];
+        
+        for (int i=0,j=1;i<n-1;i+=2){
+            if (nums[i]!=nums[j]){
+                return nums[i];
+            }
+            j+=2;
+        }
+        return nums[n-1];
+        
+    }
+}
